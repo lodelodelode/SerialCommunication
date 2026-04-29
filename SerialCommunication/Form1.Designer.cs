ea90b28a76f72036a44a223affc4bf0cbd50fd59
Lode Devooght <lode.devooght@student.hogent.be>
2026-04-29 11:28:05 +0200

    Titel Aangepast

--- SerialCommunication/Form1.Designer.cs
+++ SerialCommunication/Form1.Designer.cs
@@ -873,7 +873,7 @@
             this.Controls.Add(this.comboBoxPoort);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "BZL seriële communicatie Lode Devooght";
+            this.Text = "BZL seriële communicatie Lode";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl.ResumeLayout(false);
             this.tabPageInstellingen.ResumeLayout(false);
